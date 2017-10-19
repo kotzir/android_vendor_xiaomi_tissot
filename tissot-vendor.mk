d93534e4fc528f22bc344d7ec7cf66a142ad5643
SamarV-121 <samarvispute121@gmail.com>
2017-10-19 10:47:47 +0530

    tissot: Import Blobs From N2G47H.7.9.21

---
+++ tissot-vendor.mk
@@ -0,0 +1,5 @@
+# SYSTEM FILES
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/tissot/proprietary,system)
+# ROOT FILES
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/tissot/rootdir,root)
+
