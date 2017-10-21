eb46810530175fa36028d7a98282fdcab9c402a8
Sean Hoyt <seanhoyt963@gmail.com>
2017-10-21 17:23:07 +0800

    Remove ramdisk

--- tissot-vendor.mk
+++ tissot-vendor.mk
@@ -1,5 +1,2 @@
 # SYSTEM FILES
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/tissot/proprietary,system)
-# ROOT FILES
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/tissot/rootdir,root)
-
